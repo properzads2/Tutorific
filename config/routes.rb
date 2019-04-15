e133facc9a0075abf379050f0bc3469f7bde3341
@JSDunn105 <j.s.dunn@icloud.com>
2019-04-15 16:28:36 -0400

    models, migrations, routes for students and tutors and subjects

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :subjects
+  resources :tutor_subjects
+  resources :users
+  resources :student_tutor
+  resources :review
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
