7a91af9c39cefc4e10f54913c4960b819f18d79d
github username <github email address>
2019-04-16 11:31:17 -0400

    intial user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+
+  #before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
+  def after_sign_in_path_for(resource)
+
+  user_path(@user.id) #your path
+  end
+
+  
+
+
+
+
+  protected
+  def configure_permitted_parameters
+    attributes = [:role,:first_name,:last_name,:profile]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+  #  params.require(:user).permit(:role,:first_name,:last_name,:profile)
+  end
+
+
+
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  extend Devise::Models
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
+
+        has_many :subjects
+end
